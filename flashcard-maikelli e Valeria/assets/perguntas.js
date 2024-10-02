9b10ad7fd683439cade92db6cd0f0833e71b629f
maikelli <163297418+maikelli@users.noreply.github.com>
2024-10-02 08:37:30 -0300

    Add files via upload

---
+++ flashcard-maikelli e Valeria/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    categoria,'Programação',
+    pergunta,'O que é Python?',
+    resposta, 'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    categoria,'Geografia',
+    pergunta, 'Qual a capital da França?',
+    resposta, 'A capital da França é Paris'
+)
+
+criaCartao(
+    categoria,'Programação',
+    pergunta,'O que é uma função?',
+    resposta,'Uma função é um bloco de código que executa alguma tarefa'
+)
